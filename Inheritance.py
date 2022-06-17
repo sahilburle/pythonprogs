dd404d47199d3feb326e69eecbd92c603c54f5b4
Sahil Burle <65106393+sahilburle@users.noreply.github.com>
2022-06-17 16:18:40 +0530

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,31 @@
+class A:
+    def feature1(self):
+        print("Feature 1 working")
+
+    def feature2(self):
+        print("Feature 2 working")
+
+class B:                                     #inheritance.. inherting from class A
+    def feature3(self):
+        print("Feature 3 working")
+
+    def feature4(self):
+        print("Feature 4 working")
+
+class C(A,B):
+    def feature5(self):
+        print("Feature 5 working")
+
+a1 = A()
+
+a1.feature1()
+a1.feature2()
+
+b1 = B()
+
+b1.feature3()
+b1.feature4()
+
+c1 = C()
+
+c1.feature2()
