cc1a70ab622251efd74cc0a301785348041831fd
Sahil Burle <65106393+sahilburle@users.noreply.github.com>
2022-06-19 15:17:59 +0530

    Add files via upload

---
+++ MultiThreading.py
@@ -0,0 +1,24 @@
+from threading import *
+from time import sleep
+
+class Hello(Thread):
+    def run(self):
+        for i in range(5):
+            print('hello')
+            sleep(1)
+
+class Hi(Thread):
+    def run(self):
+        for i in range(5):
+            print('hi')
+
+t1 = Hello()
+t2 = Hi()
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print('bye')
