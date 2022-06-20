14b41ccf0200cb3c832e8311d8d6e4fd634db477
Sahil Burle <65106393+sahilburle@users.noreply.github.com>
2022-06-20 10:06:36 +0530

    Add files via upload

---
+++ BinarySearch.py
@@ -0,0 +1,29 @@
+def binarySearch(list, no, low, high):
+
+    if high >= low:
+
+        mid = (high + low)//2
+
+        #at mid
+        if list[mid] == no:
+            return mid
+
+        elif list[mid] > no:
+            return binarySearch(list, no, low, mid-1)
+
+        else:
+            return binarySearch(list, no, mid+1, high)
+
+    else:
+        return -1
+
+list = [2,3,4,6,7,8,9,11]
+
+no = 11
+
+result = binarySearch(list, no, 0, len(list)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print('not found')
